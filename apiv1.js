fa2848744a40e80f37e4941143f9091eba2bb1b2
Roy Martin <roy@roy-martin.com>
2015-02-05 22:37:09 -0800

    setup initial struture for api.

--- apiv1.js
+++ apiv1.js
@@ -6,11 +6,26 @@ var apiv1 = require('express').Router(),
         access_token: process.env.STRAVA_ACCESS_TOKEN
     });
 
-//get list of activities from Strava API
-apiv1.get('/strava/listActivities', function(req, res) {
+//get list of activities such as runs and workouts
+apiv1.get('/activities', function(req, res) {
     strava.athlete.activities.get({}, function(args, results) {
         res.jsonp(results);
     });
+});
+
+//combine twitter and blog posts
+apiv1.get('/thoughts', function(req, res) {
+    res.jsonp({});
+});
+
+//books, podcasts, etc
+apiv1.get('/learning', function(req, res) {
+    res.jsonp({});
+});
+
+//coding and development related updates
+apiv1.get('/coding', function(req, res) {
+    res.jsonp({});
 });
 
 module.exports = apiv1;
